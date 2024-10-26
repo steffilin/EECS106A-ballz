dc8768b3fb4dd9d8807490d6571f17b3ae914c48
steffilin <steffi2002.lin@gmail.com>
2024-10-25 18:29:55 -0700

    catcher starter, thrower clean up

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cc/ee106a/fa24/class/ee106a-ail/project/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cc/ee106a/fa24/class/ee106a-ail/ros_workspaces/project/devel;/opt/ros/eecsbot_ws/devel;/opt/ros/noetic'.split(';') if '/home/cc/ee106a/fa24/class/ee106a-ail/ros_workspaces/project/devel;/opt/ros/eecsbot_ws/devel;/opt/ros/noetic' != '' else []
