2b698ac8ffb05855c72ca5c7ff1b14284283a77d
steffilin <steffi2002.lin@gmail.com>
2024-11-16 18:15:21 -0800

    clean up

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,7 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("catchthrow")
 subdirs("ik")
 subdirs("move_arm")
 subdirs("planning")
--- lab7/build/baxter_pykdl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106a/fa24/class/ee106a-ail/project/lab7/src/baxter_pykdl
-# Build directory: /home/cc/ee106a/fa24/class/ee106a-ail/project/lab7/build/baxter_pykdl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
