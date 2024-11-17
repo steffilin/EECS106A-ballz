b10d7f618d07dc079952c4bbb0a87dd8f849079e
steffilin <steffi2002.lin@gmail.com>
2024-11-16 17:41:13 -0800

    I SEE BALLS

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,6 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH='/home/cc/ee106a/fa24/class/ee106a-ail/ros_workspaces/project/devel/lib:/opt/ros/eecsbot_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PKG_CONFIG_PATH='/home/cc/ee106a/fa24/class/ee106a-ail/ros_workspaces/project/devel/lib/pkgconfig:/opt/ros/eecsbot_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/home/cc/ee106a/fa24/class/ee106a-ail/project/build'
